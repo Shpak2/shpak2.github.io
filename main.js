b95fc93f60688220ed0cca43aae2ccbaca77d8e8
Shpak2 <zhenya19880307@gmail.com>
2019-06-09 23:04:36 +0300

    stylelint

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict'
 let timeOut;
+
 function goUp() {
   let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-  if(top>0) {
+  if (top > 0) {
     window.scrollBy(0, -100);
     timeOut = setTimeout('goUp()', 50);
-  }
-  else clearTimeout(timeOut);
+  } else clearTimeout(timeOut);
 }
